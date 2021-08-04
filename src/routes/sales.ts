5bd54fcc73043ae182fc1338eb7b1439aa1d9b23
Guimarães Mahota <guimaraesmahota@gmail.com>
2021-08-04 13:18:37 +0200

    first commit

---
+++ src/routes/sales.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+
+import {
+  get_all_orders,
+  get_order,
+  create_order,
+} from "../controllers/sales/orderController";
+
+const salesRouter = Router();
+
+salesRouter.get("/orders", get_all_orders);
+salesRouter.get("/orders/:id", get_order);
+salesRouter.post("/orders/", create_order);
+
+
+export default salesRouter;
